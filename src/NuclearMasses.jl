70c4e5a6ee9a7f56491c9d59e1a4f8e28b8a2009
Krzysztof Miernik <k.a.miernik@gmail.com>
2022-12-11 21:48:28 +0100

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.29

---
+++ src/NuclearMasses.jl
@@ -0,0 +1,5 @@
+module NuclearMasses
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using NuclearMasses
+using Test
+
+@testset "NuclearMasses.jl" begin
+    # Write your tests here.
+end
